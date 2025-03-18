a09262e4d7503ed285117ceb46f8c46af710ce73
anasdhr <a.dahiri1@campus.unimib.it>
2025-03-18 11:17:48 +0100

    fix backend deploy

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,6 @@ const cors = require('cors');
 const querystring = require('querystring');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const history = require('connect-history-api-fallback');
 
 /**
@@ -41,23 +39,9 @@ const generateRandomString = length => {
 
 const stateKey = 'spotify_auth_state';
 
-// Multi-process to utilize all CPU cores.
-if (cluster.isMaster) {
-  console.warn(`Node cluster master ${process.pid} is running`);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(
-      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`,
-    );
-  });
-} else {
   const app = express();
-
+  
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../client/build')));
 
@@ -179,4 +163,6 @@ if (cluster.isMaster) {
   app.listen(PORT, function () {
     console.warn(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
   });
-}
+
+
+  module.exports = app;
